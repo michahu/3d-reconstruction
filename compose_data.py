b938f162fa44c44f3dfdf83545ad1e196cd79eb4
julianknodt <julianknodt@gmail.com>
2020-01-08 21:23:54 -0500

    Swapping this to Mikkyhu

--- compose_data.py
+++ compose_data.py
@@ -11,38 +11,38 @@ num_sectors = 16
 num_pca_components = 3
 
 def get_sector(d):
-    if d > np.pi * -1 / 16 and d < np.pi * 1 / 16:
-      return 0
-    elif d > np.pi * 1 / 16 and d < np.pi * 3 / 16:
-      return 1
-    elif d > np.pi * 3 / 16 and d < np.pi * 5 / 16:
-      return 2
-    elif d > np.pi * 5 / 16 and d < np.pi * 7 / 16:
-      return 3
-    elif d > np.pi * 7 / 16 and d < np.pi * 9 / 16:
-      return 4
-    elif d > np.pi * 9 / 16 and d < np.pi * 11 / 16:
-      return 5
-    elif d > np.pi * 11 / 16 and d < np.pi * 13 / 16:
-      return 6
-    elif d > np.pi * 13 / 16 and d < np.pi * 15 / 16:
-      return 7
-    elif d > np.pi * 15 / 16 or d < np.pi * -15 / 16:
-      return 8
-    elif d > np.pi * -15 / 16 and d < np.pi * -13 / 16:
-      return 9
-    elif d > np.pi * -13 / 16 and d < np.pi * -11 / 16:
-      return 10
-    elif d > np.pi * -11 / 16 and d < np.pi * -9 / 16:
-      return 11
-    elif d > np.pi * -9 / 16 and d < np.pi * -7 / 16:
-      return 12
-    elif d > np.pi * -7 / 16 and d < np.pi * -5 / 16:
-      return 13
-    elif d > np.pi * -5 / 16 and d < np.pi * -3 / 16:
-      return 14
-    else:
-      return 15
+  if d > np.pi * -1 / 16 and d < np.pi * 1 / 16:
+    return 0
+  elif d > np.pi * 1 / 16 and d < np.pi * 3 / 16:
+    return 1
+  elif d > np.pi * 3 / 16 and d < np.pi * 5 / 16:
+    return 2
+  elif d > np.pi * 5 / 16 and d < np.pi * 7 / 16:
+    return 3
+  elif d > np.pi * 7 / 16 and d < np.pi * 9 / 16:
+    return 4
+  elif d > np.pi * 9 / 16 and d < np.pi * 11 / 16:
+    return 5
+  elif d > np.pi * 11 / 16 and d < np.pi * 13 / 16:
+    return 6
+  elif d > np.pi * 13 / 16 and d < np.pi * 15 / 16:
+    return 7
+  elif d > np.pi * 15 / 16 or d < np.pi * -15 / 16:
+    return 8
+  elif d > np.pi * -15 / 16 and d < np.pi * -13 / 16:
+    return 9
+  elif d > np.pi * -13 / 16 and d < np.pi * -11 / 16:
+    return 10
+  elif d > np.pi * -11 / 16 and d < np.pi * -9 / 16:
+    return 11
+  elif d > np.pi * -9 / 16 and d < np.pi * -7 / 16:
+    return 12
+  elif d > np.pi * -7 / 16 and d < np.pi * -5 / 16:
+    return 13
+  elif d > np.pi * -5 / 16 and d < np.pi * -3 / 16:
+    return 14
+  else:
+    return 15
 
 def get_camera_angle(camera, center):
   # camera: (x, y, z)
@@ -60,7 +60,7 @@ def compute_x(datum):
     pca = PCA(n_components=3)
     pca.fit(points)
     return pca.components_.flatten() # returns a 1 x 9 vector
-  
+
   def get_center(points):
     return np.mean(points, axis=0)[:2]
 
@@ -73,7 +73,7 @@ def compute_x(datum):
       sector = get_sector(d)
       ret[sector] = 1
     return ret
-  
+
   pca_components = compute_PCA_components(pts)
   camera_sectors = compute_sectors(cams, pts)
   return np.concatenate((pca_components, camera_sectors), axis=None)
@@ -81,8 +81,10 @@ def compute_x(datum):
 def compute_y(best: "Description of scene with all cameras",
               datum: "Array of Data loaded from parsed npz") -> "Discretized cam vec":
   losses = [emd(best['points'], dat['points']) for dat in datum]
-  best_idx = np.argmin(losses)
-  return compute_x(datum[best_idx])
+  best_data = datum[np.argmin(losses)]
+  print(best_data["cameras"])
+  exit()
+  return
 
 def construct_training_set():
   pass
--- emd.py
+++ emd.py
@@ -3,13 +3,20 @@ from scipy.optimize import linear_sum_assignment
 import numpy as np
 
 def emd(x1, x2):
-    assert len(x1) == len(x2)
-    N = len(x1)
-    d = cdist(x1, x2)
-    assignment = linear_sum_assignment(d)
-    return (d[assignment].sum() / N)
+  if len(x1) < len(x2):
+    print(x2.shape)
+    x1 = np.pad(x1, np.subtract(x1.shape, x2.shape)/2, mode='mean')
+  elif  len(x1) > len(x2):
+    x2 = np.pad(x2, 1, mode='mean')
+  print(x1.shape)
+  print(x2.shape)
+  exit()
+  N = len(x1)
+  d = cdist(x1, x2)
+  assignment = linear_sum_assignment(d)
+  return (d[assignment].sum() / N)
 
 if __name__ == '__main__':
-    x1 = np.array([1])[:, np.newaxis]
-    x2 = np.array([2])[:, np.newaxis]
-    emd(x1, x2)
+  x1 = np.array([1])[:, np.newaxis]
+  x2 = np.array([2])[:, np.newaxis]
+  emd(x1, x2)
