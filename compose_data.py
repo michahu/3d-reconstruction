79d70ac7df501199f34e1e39a590020c5c678afd
julianknodt <julianknodt@gmail.com>
2020-01-08 20:43:33 -0500

    Added computing the y to compose_data

--- compose_data.py
+++ compose_data.py
@@ -5,6 +5,7 @@ import numpy as np
 # need to install scikit learn
 # documentation: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
 from sklearn.decomposition import PCA
+from emd import emd
 
 num_sectors = 16
 num_pca_components = 3
@@ -24,17 +25,16 @@ def compute_x(input_data):
     # TODO: compute camera sector vector
     pass
 
-  pass
-
-def compute_y():
-  pass
+def compute_y(best: "Description of scene with all cameras",
+              datum: "Array of Data loaded from parsed npz") -> "Discretized cam vec":
+  losses = [emd(best['points'], dat['points']) for dat in datum]
+  best_idx = np.argmin(losses)
+  return compute_x(datum[best_idx])
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print("Usage: ./compose_data.py <Output from parseBundleOut.py>")
+  if len(sys.argv) < 3:
+    print("Usage: ./compose_data.py <parseBundleOut all cameras> <parseBundleOut some # of cams>")
     exit()
-  with np.load(sys.argv[1]) as data:
-    points = data['points']
-    print(points)
-    cams = data["cameras"]
-    print(cams)
+  best = np.load(sys.argv[1], allow_pickle=True)
+  data = [np.load(f, allow_pickle=True) for f in sys.argv[2:]]
+  compute_y(best, data)
